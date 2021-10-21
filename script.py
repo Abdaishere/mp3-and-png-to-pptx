eaf22679d29cdf5b4410f842449700bfc3492c22
Abdallah El belkasy <59124608+Abdaishere@users.noreply.github.com>
2021-10-21 19:23:27 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,42 @@
+from pptx import Presentation
+from pptx.util import Inches
+from pptx.enum.shapes import PP_MEDIA_TYPE
+from os import listdir
+from os.path import isfile, join
+
+
+prs = Presentation()
+src = '.\\Add here\\'
+fileNames = [f for f in listdir(src) if isfile(join(src, f))]
+
+# print files sorted
+# print(fileNames)
+
+size = len(fileNames)
+for i in range(0, size, 2):
+
+    # chose a slide layout to use 
+    slide_register = prs.slide_layouts[6]
+    slide = prs.slides.add_slide(slide_register)
+
+    # add audio to the slide
+    audio_shape = slide.shapes.add_movie(
+        src + fileNames[i],
+        left=Inches(4.17),
+        top=Inches(6.74),
+        width=Inches(1.67),
+        height=Inches(0.76),
+        poster_frame_image=None,
+        mime_type='audio/mp3')
+
+    # audio_shape.media_type = PP_MEDIA_TYPE.AUDIO
+
+    img_path = src + fileNames[i + 1]
+
+    # location of the image in the slide
+    left = top = Inches(0)
+
+    pic = slide.shapes.add_picture(img_path, left, top)
+    
+
+prs.save("Lecture.pptx")
